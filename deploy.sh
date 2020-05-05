64dfd36041bae1f9b4efa4050c1e18084092dc74
Shohei Hosaka <shohei.hosaka@gmail.com>
2020-05-05 22:41:24 +0900

    modify the travis.yml and create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t shoheihosaka/udemy-multi-client:latest -t shoheihosaka/udemy-multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t shoheihoaska/udemy-multi-server:latest -t shoheihosaka/udemy-multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t shoheihosaka/udemy-multi-worker:latest -t shoheihosaka/udemy-multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push shoheihosaka/udemy-multi-client:latest
+docker push shoheihosaka/udemy-multi-server:latest
+docker push shoheihosaka/udemy-multi-worker:latest
+
+docker push shoheihosaka/udemy-multi-client:$SHA
+docker push shoheihosaka/udemy-multi-server:$SHA
+docker push shoheihosaka/udemy-multi-worker:$SHA
+
+
+kubectl apply -f k8s
+kubectl set image deploymetns/client-deployment client=shoheihosaka/udemy-multi-client:$SHA
+kubectl set image deploymetns/server-deployment server=shoheihosaka/udemy-multi-server:$SHA
+kubectl set image deploymetns/worker-deployment worker=shoheihosaka/udemy-multi-worker:$SHA
